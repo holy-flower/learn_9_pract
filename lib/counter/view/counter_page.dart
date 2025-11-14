564e53d3b50ee8db077fabc7b08f51f8658b4a2a
holy-flower <prodolnova04@mail.ru>
2025-11-14 13:32:18 +0300

    исправление импортов

--- lib/counter/view/counter_page.dart
+++ lib/counter/view/counter_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../cubit/counter_cubit.dart';
+import 'counter_view.dart';
 
 /// {@template counter_page}
 /// A [StatelessWidget] which is responsible for providing a
